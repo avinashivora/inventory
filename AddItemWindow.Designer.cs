2a48b8bc01542f5ea77672d7ddba6784b2884468
avinashivora <avinashimvora@gmail.com>
2024-03-18 12:34:43 +0530

    add item win form created

---
+++ AddItemWindow.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace inventory
+{
+    partial class AddItemWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.item_name = new System.Windows.Forms.TextBox();
+            this.price = new System.Windows.Forms.TextBox();
+            this.item_company = new System.Windows.Forms.TextBox();
+            this.item_desc = new System.Windows.Forms.TextBox();
+            this.item_category = new System.Windows.Forms.ComboBox();
+            this.new_item_btn = new System.Windows.Forms.Button();
+            this.inventoryDataSet = new inventory.inventoryDataSet();
+            this.barcodeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.barcodeTableAdapter = new inventory.inventoryDataSetTableAdapters.BarcodeTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barcodeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Item Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(332, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Item Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(334, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Company Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(231, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Enter Category";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 37);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Enter Price";
+            // 
+            // item_name
+            // 
+            this.item_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item_name.Location = new System.Drawing.Point(395, 24);
+            this.item_name.Name = "item_name";
+            this.item_name.Size = new System.Drawing.Size(550, 39);
+            this.item_name.TabIndex = 5;
+            // 
+            // price
+            // 
+            this.price.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price.Location = new System.Drawing.Point(395, 224);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(150, 39);
+            this.price.TabIndex = 6;
+            // 
+            // item_company
+            // 
+            this.item_company.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item_company.Location = new System.Drawing.Point(395, 124);
+            this.item_company.Name = "item_company";
+            this.item_company.Size = new System.Drawing.Size(550, 39);
+            this.item_company.TabIndex = 8;
+            // 
+            // item_desc
+            // 
+            this.item_desc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item_desc.Location = new System.Drawing.Point(395, 74);
+            this.item_desc.Name = "item_desc";
+            this.item_desc.Size = new System.Drawing.Size(550, 39);
+            this.item_desc.TabIndex = 9;
+            // 
+            // item_category
+            // 
+            this.item_category.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.item_category.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item_category.FormattingEnabled = true;
+            this.item_category.Location = new System.Drawing.Point(395, 174);
+            this.item_category.Name = "item_category";
+            this.item_category.Size = new System.Drawing.Size(350, 40);
+            this.item_category.TabIndex = 10;
+            // 
+            // new_item_btn
+            // 
+            this.new_item_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_item_btn.Location = new System.Drawing.Point(297, 338);
+            this.new_item_btn.Name = "new_item_btn";
+            this.new_item_btn.Size = new System.Drawing.Size(160, 44);
+            this.new_item_btn.TabIndex = 11;
+            this.new_item_btn.Text = "Add Item";
+            this.new_item_btn.UseVisualStyleBackColor = true;
+            this.new_item_btn.Click += new System.EventHandler(this.new_item_btn_Click);
+            // 
+            // inventoryDataSet
+            // 
+            this.inventoryDataSet.DataSetName = "inventoryDataSet";
+            this.inventoryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // barcodeBindingSource
+            // 
+            this.barcodeBindingSource.DataMember = "Barcode";
+            this.barcodeBindingSource.DataSource = this.inventoryDataSet;
+            // 
+            // barcodeTableAdapter
+            // 
+            this.barcodeTableAdapter.ClearBeforeFill = true;
+            // 
+            // AddItemWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 467);
+            this.Controls.Add(this.new_item_btn);
+            this.Controls.Add(this.item_category);
+            this.Controls.Add(this.item_desc);
+            this.Controls.Add(this.item_company);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.item_name);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddItemWindow";
+            this.Text = "Add New Item";
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barcodeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox item_name;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.TextBox item_company;
+        private System.Windows.Forms.TextBox item_desc;
+        private System.Windows.Forms.ComboBox item_category;
+        private System.Windows.Forms.Button new_item_btn;
+        private inventoryDataSet inventoryDataSet;
+        private System.Windows.Forms.BindingSource barcodeBindingSource;
+        private inventoryDataSetTableAdapters.BarcodeTableAdapter barcodeTableAdapter;
+    }
+}
+
---
+++ AddItemWindow.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace inventory
+{
+    public partial class AddItemWindow : Form
+    {
+        string conStr = ConfigurationManager.ConnectionStrings["inventory.Properties.Settings.inventoryCon"].ConnectionString;
+        public AddItemWindow()
+        {
+            InitializeComponent();
+            PopulateComboBox();
+        }
+        private void PopulateComboBox()
+        {
+            string query = "SELECT c_name FROM category";
+            using(SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string category = reader["c_name"].ToString().Trim();
+                            item_category.Items.Add(category);
+                        }
+                    }
+                }
+            }
+            
+        }
+        private void new_item_btn_Click(object sender, EventArgs e)
+        {
+            string alert = "";
+            float i_price=0;
+
+            if (string.IsNullOrEmpty(item_name.Text.Trim()))
+            {
+                alert += "Item Name is Required! \n";
+            }
+            if (item_category.SelectedIndex == -1)
+            {
+                alert += "Item Category needs to be selected! \n";
+            }
+            if (string.IsNullOrEmpty(price.Text.Trim()))
+            {
+                alert += "Item Price is Required! \n";
+            }
+            else
+            {
+                try
+                {
+                    i_price = float.Parse(price.Text.Trim());
+                }
+                catch
+                {
+                    alert += "Item Price needs to be Numeric! \n";
+                }
+            }
+            if (!string.IsNullOrEmpty(alert))
+            {
+                MessageBox.Show(alert, "Empty Fields", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                using(SqlConnection con = new SqlConnection(conStr))
+                {
+                    SqlCommand insert = new SqlCommand("INSERT INTO item (item_name, desc_item, company_name, category_id, price) VALUES (@item_name, @desc_item, @company_name, @category_id, @price)", con);
+                    con.Open();
+                    SqlCommand c_id = new SqlCommand($"SELECT c_id FROM category WHERE c_name = @c_name", con);
+                    c_id.Parameters.AddWithValue("@c_name", item_category.SelectedItem.ToString());
+                    int cid = 0;
+                    object result = c_id.ExecuteScalar();
+                    if (result != null)
+                    {
+                        cid =  int.Parse(result.ToString());
+                    }
+                    else
+                    {
+                        MessageBox.Show("Database Connection Error!");
+                    }
+                    insert.Parameters.AddWithValue("@item_name", item_name.Text.Trim());
+                    insert.Parameters.AddWithValue("@desc_item", item_desc.Text.Trim());
+                    insert.Parameters.AddWithValue("@company_name", item_company.Text.Trim());
+                    insert.Parameters.AddWithValue("@category_id", cid);
+                    insert.Parameters.AddWithValue("@price", i_price);
+                    try
+                    {
+                        insert.ExecuteNonQuery();
+                        MessageBox.Show("Item Added to Catalogue Successfully", "Item Created");
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Item Created");
+                    }
+                    item_name.Text = "";
+                    item_desc.Text = "";
+                    item_company.Text = "";
+                    price.Text = "";
+                    item_category.SelectedIndex = -1;
+                    con.Close();
+                }
+            }
+        }
+    }
+}
